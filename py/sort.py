c24d69036022c1469b96972a2d3ba87b5069d99f
Varad Dixit <23BBT035@spt.pdpu.ac.in>
2024-05-15 17:16:34 +0530

    sorting rework + add cpython stuff to gitignore.

--- py/sort.py
+++ py/sort.py
@@ -1,59 +1,34 @@
-# sortedto: 
-# param is a list
-# returns an int; index of first unsorted element
-# if return == length of list, list is sorted
-
-def sortedto(mylist):
+def sortedto(mylist: list[int]) -> int:
 
     upto = len(mylist)
 
     for i in range(len(mylist)-1):
-
         if mylist[i] > mylist[i+1]:
             upto = i+1
             break
-    
-    return upto
 
-def spsortedto(mylist):
-    
-    ref = sorted(mylist)
-
-    for i in range(len(mylist)):
-        if mylist[i] != ref[i]:
-            upto = i
-            break
-    
     return upto
 
 def issorted(mylist):
-    
-    # return mylist == sorted(mylist)
-    
-    var = True
-    
-    if sortedto(mylist) < len(mylist):
-        var = False
 
-    if len(mylist) <= 1:
-        var = True
-    
-    return var
+    # return mylist == sorted(mylist)
+
+    return sortedto(mylist) == len(mylist)
 
 
 def minimum(mylist):
-    
+
     minindex = 0
 
     for i in range(len(mylist)):
-        
+
         if mylist[minindex] > mylist[i]:
             minindex = i
-    
-    return minindex
+
+    return (minindex, mylist[minindex])
 
 def maximum(mylist):
-    
+
     maxindex = 0
 
     for i in range(len(mylist)):
@@ -61,4 +36,4 @@ def maximum(mylist):
         if mylist[maxindex] <= mylist[i]:
             maxindex = i
 
-    return maxindex
+    return (maxindex, mylist[maxindex])
